25d421f164d4db49ecd989e2c81258e04b946f04
kirillrep33 <kirillrep33@gmail.com>
2026-03-17 10:50:00 +0300

    2

--- NutritionLog/App.swift
+++ NutritionLog/App.swift
@@ -2,11 +2,19 @@ import SwiftUI
 
 @main
 struct NutritionLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RootView()
+        }
+    }
+}
+
+struct RootView: View {
     @AppStorage("hasCompletedOnboarding") private var hasCompletedOnboarding = false
     @StateObject private var nutritionStore = NutritionStore()
     
-    var body: some Scene {
-        WindowGroup {
+    var body: some View {
+        Group {
             if hasCompletedOnboarding {
                 ContentView()
                     .environmentObject(nutritionStore)
